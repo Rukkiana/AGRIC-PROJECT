f1499dcf5054c9f2c37de070420cc662af925a21
Rukkiana <rukayyaabdulkadri@gmail.com>
2024-06-06 12:07:27 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: { 
+      boxShadow: {
+        'custom': '0px 3.56px 6.76px 0.89px #B1B8A366',
+      },
+    },
+  },
+  plugins: [],
+}
+
